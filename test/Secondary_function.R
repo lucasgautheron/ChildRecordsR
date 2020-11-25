b7f697a5a7ae5c266e4607eafcb5c7817fc470c7
Alejandrina Cristia <alejandrinacristia@Alejandrinas-MacBook-Air.local>
2020-11-25 09:35:11 +0100

    separation method

--- test/Secondary_function.R
+++ test/Secondary_function.R
@@ -1,8 +1,6 @@
 
-library(reshape)
-library(ggplot2)
-library(GGally)
-# library("ggpubr")
+
+
 
 
 
@@ -120,79 +118,4 @@ data_to_OneHotEnc <- function(data){
   data$composit <- rez
   data
 }
-
-
-
-# row_to_OneHotEnc <- function(row){
-#   
-#   if (rowSums(row)==0){ 
-#     rez=0
-#   }else{
-#     if(rowSums(row)>1) {
-#       rez =NA
-#     }else{
-#       if(row$CHI>=1) rez = 1
-#       if(row$FEM>=1) rez = 2
-#       if(row$MAL>=1) rez = 3
-#       if(row$OCH>=1) rez = 4
-#     }
-#   }
-#   rez
-#   
-# }
-
-
-library(gridExtra)
-setClass("raterCompCR")
-setGeneric("plot")
-setMethod(
-  "plot",signature = "raterCompCR",
-  
-  function(x){
-    data <- x$rating_by_comp
-    compo.names <- names(data)
-    
-    for ( compo in 1:length(data)){
-      
-      a <- ggpairs(data[[compo]][complete.cases(data[[compo]]),-1])
-      
-      data.tmp <- melt(data[[compo]][complete.cases(data[[compo]]),],id="time.seq")
-      b  <- ggplot(data.tmp,aes(x=time.seq,y=value))+
-        # geom_line(alpha=0.5)+
-        geom_ribbon(aes(fill=variable, ymax=pmax(value,0), ymin=0), alpha=0.5)+
-        # geom_density_ridges(alpha=0.6, stat="binline", bins=1)+
-        theme(legend.position = c(0.2, 0.8), 
-              legend.text = element_text(size = 10),
-              legend.title = element_blank())
-      
-      # print(a)
-      # print(b)
-      grid.arrange(
-        ggmatrix_gtable(a),
-        b,
-        nrow=1,
-        top = compo.names[compo]
-      )
-      
-      
-    }
-  }
-)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
