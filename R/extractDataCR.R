0c7f55ecf63a8ae6fb31de2e0a81ba50bfd5737b
alsonicr <nicolas.r.rochat@gmail.com>
2021-01-04 15:56:13 +0100

    can not use lena overlap method

--- R/extractDataCR.R
+++ R/extractDataCR.R
@@ -5,6 +5,7 @@
 #'
 #' @param ChildRecordings : a ChildRecordings class
 #' @param set.type : a string containing th name of the annotator present in the "set" column meta
+#' @param LENA.OL : add the LENA overlap method to handle onset and offset
 #'
 #' @return : a data.frame
 #'
@@ -13,7 +14,7 @@
 #'
 #'
 #'
-extractDataCR <- function(ChildRecordings,set.type) {
+extractDataCR <- function(set.type,LENA.OL = F,ChildRecordings) {
 
   if(!is(ChildRecordings, "ChildRecordings")){
     print("ChildRecordings value is not a ChildRecordings class retrun null result")
@@ -27,7 +28,11 @@ extractDataCR <- function(ChildRecordings,set.type) {
   ### Data extraction loop
   data <- data.frame()
   for (row in 1:nrow(all.meta)) {
-    temps.data <- file.openner(all.meta[row, ],ChildRecordings)
+    if (LENA.OL){
+      temps.data <- LENA.overlap(all.meta[row, ],ChildRecordings)
+    }else{
+      temps.data <- file.openner(all.meta[row, ],ChildRecordings)
+    }
 
     # bind data
     data <- rbind(data, temps.data)
