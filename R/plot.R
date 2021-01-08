682462849235ac6fa0377efaa7460d3073502225
alsonicr <nicolas.r.rochat@gmail.com>
2021-01-08 10:29:18 +0100

    remove library

--- R/plot.R
+++ R/plot.R
@@ -1,9 +1,9 @@
 
-library(reshape)
-library(ggplot2)
+# library(reshape)
+# library(ggplot2)
 
 
-#' 
+#'
 #'
 #'
 #'
@@ -17,30 +17,30 @@ library(ggplot2)
 #'
 #'
 
-library(GGally)
-library(gridExtra)
+# library(GGally)
+# library(gridExtra)
 setClass("raterCompCR")
 setGeneric("plot")
 setMethod(
   "plot",signature = "raterCompCR",
-  
+
   function(x){
     data <- x$rating_by_comp
     compo.names <- names(data)
-    
+
     for ( compo in 1:length(data)){
-      
+
       a <- ggpairs(data[[compo]][complete.cases(data[[compo]]),-1])
-      
+
       data.tmp <- melt(data[[compo]][complete.cases(data[[compo]]),],id="time.seq")
       b  <- ggplot(data.tmp,aes(x=time.seq,y=value))+
         # geom_line(alpha=0.5)+
         geom_ribbon(aes(fill=variable, ymax=pmax(value,0), ymin=0), alpha=0.5)+
         # geom_density_ridges(alpha=0.6, stat="binline", bins=1)+
-        theme(legend.position = c(0.2, 0.8), 
+        theme(legend.position = c(0.2, 0.8),
               legend.text = element_text(size = 10),
               legend.title = element_blank())
-      
+
       # print(a)
       # print(b)
       grid.arrange(
@@ -49,8 +49,8 @@ setMethod(
         nrow=1,
         top = compo.names[compo]
       )
-      
-      
+
+
     }
   }
 )
