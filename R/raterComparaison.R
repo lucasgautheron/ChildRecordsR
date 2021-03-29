dad5b516f75ab7af9da586eea149dde33d4baca6
alsonicr <nicolas.r.rochat@gmail.com>
2021-03-29 12:54:20 +0200

    change name function

--- R/raterComparaison.R
+++ R/raterComparaison.R
@@ -21,7 +21,7 @@
 #' search = find.rating.segment(CR, "aiku/namibie_aiku_20160715_1.wav")
 #' rez = aggregate.rating(search, CR, cut=0.100, verbose=T)
 #'
-#' comparaison = raterComparaison(rez)
+#' comparaison = compare.rating(rez)
 #' plot(comparaison)
 
 
@@ -29,7 +29,7 @@
 
 
 
-raterComparaison <- function(raterData){
+compare.rating <- function(raterData){
 
   if(!is(raterData, "raterData")){
     print(paste( substitute(raterData), "is not a raterData class retrun null result"))
@@ -38,7 +38,7 @@ raterComparaison <- function(raterData){
 
 
   raters <- raterData$args$ratersID
-  global.reliability <- reliability(raterData,summary=F)
+  global.reliability <- get.reliability(raterData,summary=F)
   global.reliability <- global.reliability$reliability[["composit"]]
 
   rater.result <- list()
@@ -59,7 +59,7 @@ raterComparaison <- function(raterData){
 
       ctt.data <- cbind(ctt.data,data.frame(ratting))
 
-      tmp<-SDT.raterData(raterData,c(rater,comp.rater),plot=F,summary=F)
+      tmp<-get.classification(raterData,c(rater,comp.rater),plot=F,summary=F)
       # print(tmp)
       sdt.list[[comp.rater]]<-tmp
     }
